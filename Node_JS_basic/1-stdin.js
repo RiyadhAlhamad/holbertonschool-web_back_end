07248c2ea30578fbd309eb27ade0d28454c98783
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-09-03 19:19:42 +0300

    edit 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,4 +1,3 @@
-// 1-stdin.js
 process.stdout.write("Welcome to Holberton School, what is your name?\n");
 
 process.stdin.on("data", (data) => {
@@ -6,7 +5,6 @@ process.stdin.on("data", (data) => {
   process.stdout.write(`Your name is: ${name}\n`);
 });
 
-// يطبع الرسالة عند نهاية البرنامج (ctrl+d أو pipe ينتهي)
 process.on("exit", () => {
   process.stdout.write("This important software is now closing\n");
 });
