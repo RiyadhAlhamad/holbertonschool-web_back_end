cf7e70a19b944303870d7376f276f99133e1ddef
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-09-03 23:41:48 +0300

    Edit 6-http_express.js

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -1,13 +1,12 @@
 // 6-http_express.js
 const express = require('express');
+
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello Holberton School!');
+  res.status(200).send('Hello Holberton School!');
 });
 
-if (require.main === module) {
-  app.listen(1245);
-}
+app.listen(1245, () => {});
 
 module.exports = app;
