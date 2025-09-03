783cd80d54a524463d74c9a0a572e5b75ea71ddb
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-09-03 18:09:25 +0300

    add task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+// 1-stdin.js
+process.stdout.write("Welcome to Holberton School, what is your name?\n");
+
+process.stdin.on("data", (data) => {
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
+});
+
+// يطبع الرسالة عند نهاية البرنامج (ctrl+d أو pipe ينتهي)
+process.on("exit", () => {
+  process.stdout.write("This important software is now closing\n");
+});
