554450ede4cfe892ded485da1fe2c1f26ebef8ea
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-09-03 19:44:18 +0300

    edit 1-stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,16 +1,14 @@
-#!/usr/bin/node
-
-// Print the welcome message
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Event listener for user input
-process.stdin.on('data', (data) => {
-  const input = data.toString().trim();
-  // Print the user input
-  process.stdout.write(`Your name is: ${input}\n`);
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+  }
 });
 
-// Event listener when the program ends (EOF / Ctrl+D or echo pipe)
 process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
