7b165bb133f47af2b4d4cee0c10fcc1744057041
RiyadhAlhamad <112124978+RiyadhAlhamad@users.noreply.github.com>
2025-09-03 23:36:49 +0300

    Edit 6-http_express.js

--- Node_JS_basic/6-http_express.js
+++ Node_JS_basic/6-http_express.js
@@ -2,13 +2,12 @@
 const express = require('express');
 const app = express();
 
-// define the route
 app.get('/', (req, res) => {
   res.send('Hello Holberton School!');
 });
 
-// listen on port 1245
-app.listen(1245);
+if (require.main === module) {
+  app.listen(1245);
+}
 
-// export app variable
 module.exports = app;
